00586f8c04646208ac3cbbfaa82b40e653618d32
Joel del Castillo <joel.del.castillo09@gmail.com>
2022-04-11 10:25:33 -0500

    WIFI connection to make a self accessPoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+
 int value = 0;
+const char *ssid = "Light";
+const char *password = "";
 
 void setup()
 {
   Serial.begin(9600);
-
+  Serial.print("Setting AP (Access Point)…");
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
   // put your setup code here, to run once:
 }
 
